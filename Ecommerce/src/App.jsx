ba1cff80a6d1372045a1fab5638223f0e0d12576
Mahir khan <mahirmahir8951@gmail.com>
2025-04-20 18:23:54 +0530

    Ecommerce Website using React

--- Ecommerce/src/App.jsx
+++ Ecommerce/src/App.jsx
@@ -4,6 +4,11 @@ import { useEffect, useState } from "react"
 import Banner from "./Components/Banner/Banner";
 import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
+import Products from "./Components/Products/Products";
+import TopProducts from "./Components/TopProducts/TopProducts";
+import Subscribe from "./Components/Subscribe/Subscribe";
+import Testimonials from "./Components/Testimonials/Testimonials";
+import Footer from "./Components/Footer/Footer";
 
 
 function App() {
@@ -26,8 +31,13 @@ function App() {
     <>
       <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
         <Navbar handleOrderPopup={handleOrderPopup} />
-        <Hero/>
+        <Hero handleOrderPopup={handleOrderPopup}/>
+        <Products/>
+        <TopProducts handleOrderPopup={handleOrderPopup}/>
         <Banner />
+        <Subscribe/>
+        <Testimonials/>
+        <Footer/>
       </div>
     </>
   )
